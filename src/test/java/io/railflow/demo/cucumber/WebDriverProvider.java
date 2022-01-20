b840e637a5d57b6bd286b4a70d53e0545db82e58
Sergey Oplavin <sergey.oplavin@agiletestware.com>
2022-01-20 17:46:21 +0300

    Add WebDriverManager

--- src/test/java/io/railflow/demo/cucumber/WebDriverProvider.java
+++ src/test/java/io/railflow/demo/cucumber/WebDriverProvider.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class WebDriverProvider {
 	private static final ThreadLocal<WebDriver> CURRENT_DRIVER = new ThreadLocal<>();
 
 	public static synchronized WebDriver getWebDriver() {
 		WebDriver webDriver = CURRENT_DRIVER.get();
 		if (webDriver == null) {
-			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			final ChromeOptions options = new ChromeOptions();
 			options.addArguments("--start-fullscreen");
 			webDriver = new ChromeDriver(options);
